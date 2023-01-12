cd8b71a1edbe8097e9afc85896541ed430c2bc91
TheCuddlyBear <basvthazeveld@outlook.com>
2023-01-12 11:20:32 +0100

    Add project files.

---
+++ Sketchy/SchetsControl.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketchy
+{
+    public class SchetsControl : UserControl
+    {
+        private Schets schets;
+        private Color penkleur;
+
+        public Color PenKleur
+        {
+            get { return penkleur; }
+        }
+        public Schets Schets
+        {
+            get { return schets; }
+        }
+        public SchetsControl()
+        {
+            this.BorderStyle = BorderStyle.Fixed3D;
+            this.schets = new Schets();
+            this.Paint += this.teken;
+            this.Resize += this.veranderAfmeting;
+            this.veranderAfmeting(null, null);
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+        }
+        private void teken(object o, PaintEventArgs pea)
+        {
+            schets.Teken(pea.Graphics);
+        }
+        private void veranderAfmeting(object o, EventArgs ea)
+        {
+            schets.VeranderAfmeting(this.ClientSize);
+            this.Invalidate();
+        }
+        public Graphics MaakBitmapGraphics()
+        {
+            Graphics g = schets.BitmapGraphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            return g;
+        }
+        public void Schoon()
+        {
+            schets.Schoon();
+            this.Invalidate();
+        }
+        public void Roteer()
+        {
+            schets.VeranderAfmeting(new Size(this.ClientSize.Height, this.ClientSize.Width));
+            schets.Roteer();
+            this.Invalidate();
+        }
+        public void VeranderKleur(ComboBox box)
+        {
+            string kleurNaam = (box.Text);
+            penkleur = Color.FromName(kleurNaam);
+        }
+        public void VeranderKleurViaMenu()
+        {
+            //string kleurNaam = ((ToolStripMenuItem)obj).Text;
+            penkleur = Color.FromName("black");
+        }
+    }
+}
---
+++ Sketchy/SchetsForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sketchy
+{
+    public partial class SchetsForm : Form
+    {
+
+        bool vast;
+        ISchetsTool[] deTools = { new PenTool()
+                                , new LijnTool()
+                                , new RechthoekTool()
+                                , new VolRechthoekTool()
+                                , new TekstTool()
+                                , new GumTool()
+                                };
+        ISchetsTool huidigeTool;
+        String[] deKleuren = { "Black", "Red", "Green", "Blue", "Yellow", "Magenta", "Cyan" };
+        public SchetsForm()
+        {
+            InitializeComponent();
+            this.huidigeTool = deTools[0];
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            schetsControl1.Roteer();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            schetsControl1.Schoon();
+        }
+
+        private void SchetsForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.huidigeTool = deTools[0];
+        }
+
+        private void schetsControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            vast = true;
+            this.huidigeTool.MuisVast(schetsControl1, e.Location);
+        }
+
+        private void schetsControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (vast) huidigeTool.MuisDrag(schetsControl1, e.Location);
+        }
+
+        private void schetsControl1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (vast)
+                huidigeTool.MuisLos(schetsControl1, e.Location);
+            vast = false;
+        }
+
+        private void schetsControl1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            huidigeTool.Letter(schetsControl1, e.KeyChar);
+        }
+    }
+}
